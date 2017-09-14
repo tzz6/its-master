56e5e0d990192c4800c483bdf94822701d293c7d
tzz6 <291232797@qq.com>
2017-09-14 15:53:38 +0800

    排序

---
+++ its-test/src/main/java/com/its/test/util/TestSortUtil.java
@@ -0,0 +1,86 @@
+package com.its.test.util;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+import com.its.common.utils.SortUtil;
+
+public class TestSortUtil {
+
+	/** 1.选择排序 */
+	@Test
+	public void testSelectSorter() {
+		int[] num = randomNumber(20);
+		print(num);
+		print(SortUtil.selectSorter(num));
+	}
+
+	/** 2.冒泡排序 */
+	@Test
+	public void testBubbleSorter() {
+		int[] num = randomNumber(20);
+		print(num);
+		print(SortUtil.bubbleSorter(num));
+	}
+
+	/** 3.快速排序 */
+	@Test
+	public void testQuickSorter() {
+		int[] num = randomNumber(20);
+		print(num);
+		print(SortUtil.quickSorter(num, 0, num.length - 1));
+	}
+
+	/** 4.插入排序 */
+	@Test
+	public void testInsetSorter() {
+		int[] num = randomNumber(20);
+		print(num);
+		print(SortUtil.insetSorter(num));
+	}
+
+	/** 5.希尔排序 */
+	@Test
+	public void testShellSorter() {
+		int[] num = randomNumber(20);
+		print(num);
+		print(SortUtil.shellSorter(num, num.length - 1));
+	}
+
+	/** 6.归并排序 */
+	@Test
+	public void testMergeSort() {
+		int[] num = randomNumber(20);
+		print(num);
+		print(SortUtil.mergeSort(num, 0, num.length - 1));
+	}
+
+	/** 7.堆排序 */
+	@Test
+	public void testHeapSort() {
+		int[] num = randomNumber(20);
+		print(num);
+		print(SortUtil.heapSort(num));
+	}
+
+	/** 产生number个随机数 */
+	public int[] randomNumber(int number) {
+		int[] num = new int[number];
+		Random random = new Random();
+		for (int i = 0; i < num.length; i++) {
+			// num[i] = (int) (Math.random() * 100);
+			// 产生100以为的随机数
+			num[i] = random.nextInt(100);
+		}
+		return num;
+	}
+
+	/** 输出 */
+	public void print(int[] num) {
+		for (int i = 0; i < num.length - 1; i++) {
+			System.out.print(num[i] + ",");
+		}
+		System.out.println(num[num.length - 1]);
+	}
+}
--- its-test/src/main/java/com/its/test/util/xml/TestXmlUtil.java
+++ its-test/src/main/java/com/its/test/util/xml/TestXmlUtil.java
@@ -1,5 +1,6 @@
 package com.its.test.util.xml;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.dom4j.DocumentException;
@@ -7,26 +8,83 @@ import org.junit.Test;
 
 import com.its.common.utils.xml.XmlUtil;
 
-
 public class TestXmlUtil {
-	
+
 	@Test
 	public void testXmlToMap() {
-		String xml = "<?xml version='1.0' encoding='utf-8' ?>" 
-				+ "<country>" 
-				+ "<id>1</id>"
-				+ "<createDate>2016-03-10 09:36:01</createDate>" 
-				+ "<name>中国</name>" 
-				+ "<provinces>" 
-				+ "<province><name>广东省</name><provCity>广州市</provCity><citys><city><name>白云区</name></city><city><name>xx区</name></city></citys></province>" 
-				+ "<province><name>湖南省</name><provCity>长沙市</provCity><citys><city><name>xx区1</name></city><city><name>xx区2</name></city></citys></province>" 
-				+ "</provinces>" 
-				+ "</country>";
+		String xml = "<?xml version='1.0' encoding='utf-8' ?>" + "<country>" + "<id>1</id>"
+				+ "<createDate>2016-03-10 09:36:01</createDate>" + "<name>中国</name>" + "<provinces>"
+				+ "<province><name>广东省</name><provCity>广州市</provCity><citys><city><name>白云区</name></city><city><name>xx区</name></city></citys></province>"
+				+ "<province><name>湖南省</name><provCity>长沙市</provCity><citys><city><name>xx区1</name></city><city><name>xx区2</name></city></citys></province>"
+				+ "</provinces>" + "</country>";
 		try {
 			Map<String, Object> map = XmlUtil.xmlToMap(xml);
-			for (Map.Entry<String, Object> entry : map.entrySet()){
+			for (Map.Entry<String, Object> entry : map.entrySet()) {
 				System.out.println(entry.getKey() + "---" + entry.getValue());
 			}
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testXmlToMapMax() {
+		String xml = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/'>"
+				+ "<soapenv:Header/>" 
+					+ "<soapenv:Body>"
+					+ "<xav:XAVResponse xmlns:xav='http://www.ups.com/XMLSchema/XOLTWS/xav/v1.0'>"
+						+ "<common:Response xmlns:common='http://www.ups.com/XMLSchema/XOLTWS/Common/v1.0'>"
+							+ "<common:ResponseStatus>" + "<common:Code>1</common:Code>"
+							+ "<common:Description>Success</common:Description>" 
+						+ "</common:ResponseStatus>"
+						+ "<common:TransactionReference>"
+							+ "<common:CustomerContext>Your Customer Context</common:CustomerContext>"
+						+ "</common:TransactionReference>" + "</common:Response>" + "<xav:AmbiguousAddressIndicator/>"
+						+ "<xav:AddressClassification>" 
+							+ "<xav:Code>0</xav:Code>"
+							+ "<xav:Description>Unknown</xav:Description>" 
+						+ "</xav:AddressClassification>" 
+						+ "<xav:Candidate>"
+							+ "<xav:AddressClassification>" + "<xav:Code>0</xav:Code>"
+							+ "<xav:Description>Unknown</xav:Description>" + "</xav:AddressClassification>"
+							+ "<xav:AddressKeyFormat>" + "<xav:AddressLine>3500-3598 MORRIS AVE</xav:AddressLine>"
+							+ "<xav:PoliticalDivision2>MACON</xav:PoliticalDivision2>"
+							+ "<xav:PoliticalDivision1>GA</xav:PoliticalDivision1>"
+							+ "<xav:PostcodePrimaryLow>31204</xav:PostcodePrimaryLow>"
+							+ "<xav:PostcodeExtendedLow>3226</xav:PostcodeExtendedLow>"
+							+ "<xav:Region>MACON GA 31204-3226</xav:Region>" + "<xav:CountryCode>US</xav:CountryCode>"
+							+ "</xav:AddressKeyFormat>" 
+						+ "</xav:Candidate>" 
+						+ "<xav:Candidate>" 
+							+ "<xav:AddressClassification>"
+								+ "<xav:Code>0</xav:Code>" + "<xav:Description>Unknown</xav:Description>"
+							+ "</xav:AddressClassification>" 
+							+ "<xav:AddressKeyFormat>"
+								+ "<xav:AddressLine>3501-3599 MORRIS AVE</xav:AddressLine>"
+								+ "<xav:PoliticalDivision2>MACON</xav:PoliticalDivision2>"
+								+ "<xav:PoliticalDivision1>GA</xav:PoliticalDivision1>"
+								+ "<xav:PostcodePrimaryLow>31204</xav:PostcodePrimaryLow>"
+								+ "<xav:PostcodeExtendedLow>3225</xav:PostcodeExtendedLow>"
+								+ "<xav:Region>MACON GA 31204-3225</xav:Region>" + "<xav:CountryCode>US</xav:CountryCode>"
+							+ "</xav:AddressKeyFormat>" 
+						+ "</xav:Candidate>" 
+					+ "</xav:XAVResponse>" 
+					+ "</soapenv:Body>"
+				+ "</soapenv:Envelope>";
+		try {
+			Map<String, Object> map = XmlUtil.xmlToMap(xml);
+			for (Map.Entry<String, Object> entry : map.entrySet()) {
+				System.out.println(entry.getKey() + "---" + entry.getValue());
+				if (entry.getValue() instanceof HashMap) {
+					HashMap<String, Object> m = (HashMap<String, Object>) entry.getValue();
+					for (Map.Entry<String, Object> me : m.entrySet()) {
+						System.out.println(me.getKey() + "---" + me.getValue());
+					}
+				}
+			}
+			
+			System.out.println(XmlUtil.getMapValue(map, "CustomerContext"));
 		} catch (DocumentException e) {
 			e.printStackTrace();
 		}
