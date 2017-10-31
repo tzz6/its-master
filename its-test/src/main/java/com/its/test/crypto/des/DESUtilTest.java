59a0f96cecd741e6fca2e04d3f37a1484913a2db
tzz6 <291232797@qq.com>
2017-10-31 16:39:40 +0800

    jdbc.properties加密

--- its-test/src/main/java/com/its/test/crypto/des/DESUtilTest.java
+++ its-test/src/main/java/com/its/test/crypto/des/DESUtilTest.java
@@ -6,9 +6,8 @@ import org.junit.Test;
 
 import com.its.common.crypto.des.DESUtil;
 
-
 public class DESUtilTest {
-	
+
 	@Test
 	public void testDesEncrypt() {
 		String content = "中文123456";
@@ -17,11 +16,12 @@ public class DESUtilTest {
 		String decryptStr = DESUtil.decrypt(encryptStr, DESUtil.KEY);
 		System.out.println(decryptStr);
 	}
+
 	@Test
 	public void testDes() {
 		String content = "中文123456";
-		String keyStr = DESUtil.initKey("QWE!@#123qwe123");;
-		String keyStr2 = DESUtil.initKey("QWE!@#123qwe123");;
+		String keyStr = DESUtil.initKey("QWE!@#123qwe123");
+		String keyStr2 = DESUtil.initKey("QWE!@#123qwe123");
 		System.out.println("原文:" + content);
 		System.out.println("密钥:" + keyStr);
 		System.out.println("密钥:" + keyStr2);
@@ -32,5 +32,22 @@ public class DESUtilTest {
 		byte[] decryptByte = DESUtil.decrypt(DESUtil.decryptBASE64(encryptStr), key);
 		String outputStr = new String(decryptByte);
 		System.out.println("解密后:" + outputStr);
+	}
+
+	@Test
+	public void testDesEncryptJDBC() {
+		String jdbUrl = "jdbc:mysql://localhost:3306/webdemo?useUnicode=true&amp;characterEncoding=UTF8";
+		String jdbcUsername = "root";
+		String jdbcPassword = "root123";
+		String jdbUrlEncrypt = DESUtil.encrypt(jdbUrl, DESUtil.KEY);
+		String jdbcUsernameEncrypt = DESUtil.encrypt(jdbcUsername, DESUtil.KEY);
+		String jdbcPasswordEncrypt = DESUtil.encrypt(jdbcPassword, DESUtil.KEY);
+
+		System.out.println(jdbUrlEncrypt);
+		System.out.println(jdbcUsernameEncrypt);
+		System.out.println(jdbcPasswordEncrypt);
+
+		System.out.println(DESUtil.decrypt(jdbUrlEncrypt, DESUtil.KEY));
+		System.out.println(DESUtil.decrypt(jdbcUsernameEncrypt, DESUtil.KEY));
 	}
 }
---
+++ its-web/src/main/java/org/springframework/beans/factory/config/EncryptPropertyPlaceholderConfigurer.java
@@ -0,0 +1,33 @@
+package org.springframework.beans.factory.config;
+
+import com.its.common.crypto.des.DESUtil;
+
+/**
+ * 重写Spring加载Properties文件类-实现对JDBC配置进行加解密
+ *
+ */
+public class EncryptPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+
+	private String[] encryptPropNames = { "jdbc.url", "jdbc.username", "jdbc.password" };// 加密属性名单
+
+	@Override
+	protected String convertProperty(String propertyName, String propertyValue) {
+		if (isEncryptProp(propertyName)) {
+			String decryptValue = DESUtil.decrypt(propertyValue, DESUtil.KEY);
+			System.out.println("decrypt--" + decryptValue);
+			return decryptValue;
+		} else {
+			return propertyValue;
+		}
+
+	}
+
+	private boolean isEncryptProp(String propertyName) {
+		for (String encryptName : encryptPropNames) {
+			if (encryptName.equals(propertyName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
